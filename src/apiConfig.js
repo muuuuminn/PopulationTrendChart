6d26ef332b8ae797abf0317361e61ee21e77018b
muuuuminn <hiro.041310402.kt@gmail.com>
2019-03-27 00:24:22 +0900

    reset

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -3,6 +3,6 @@ module.exports = {
     demographicsAPI:
       "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
     prefecturesAPI: "https://opendata.resas-portal.go.jp/api/v1/prefectures",
-    apiKey: "SkNZ7XOnPO1NS2QaiWWdg579PLTLtodHjImoGAA2"
+    apiKey: process.env.VUE_APP_RESAS_APIKEY
   }
 };
